ca9a428b12a8d4060c124e3db84946254fef25f5
mraliu <teachmrliu@gmail.com>
2024-06-15 10:33:07 +0100

    Obstacle including clicking is added to data structure

--- main.py
+++ main.py
@@ -2,30 +2,41 @@ import tkinter as tk, random
 from tkinter import messagebox
 
 root = tk.Tk()
-root.geometry("960x720")
+root.geometry("960x800")
 root.title("Djkstra Algorithm")
 root.resizable(False, False)
+root.config(bg="White")
 
 v = tk.IntVar()
 defaultwidth = 25
 
 menu = tk.Frame(root)
 screen = tk.Frame(root)
+dataframe = tk.Frame(root)
 
-menu.place(x=0,y=0,width=150,height=600)
-screen.place(x=150,y=0,width=810,height=700)
-grid = []
+menu.place(x=0,y=0,width=150,height=650)
+screen.place(x=150,y=0,width=810,height=650)
+dataframe.place(x=0,y=650,width=960,height=150)
+
+
+# Data
+grid = [] # Visual of obstacles
+obstacles = [] # Ostacles
 
 def clearmap():
     global screen
     screen.destroy()
     screen = tk.Frame(root)
-    screen.place(x=150,y=0,width=810,height=700)
+    screen.place(x=150,y=0,width=810,height=650)
 
 # When click creates boundary
 def addnode(item):
     r, c = item
     grid[r][c].config(bg="Grey") if grid[r][c].cget("background") == "SystemButtonFace" else grid[r][c].config(bg="SystemButtonFace")
+    coord = r*width + c
+    obstacles.append(coord)   
+    lbl_data.delete("1.0", tk.END)
+    lbl_data.insert(tk.END, obstacles)
         
 def makemap():
     global width, grid, screen
@@ -47,11 +58,13 @@ def makemap():
         grid.append(temprow)
 
 def genobstacles():
-    obstacles = []
-    totalmapsize = (width * width)-1
+    global obstacles, lbl_data, lbl_data2
+    obstacles.clear()
+    
+    totalmapsize = (width * width) - 1
     if not ent_size.get().isnumeric():
         messagebox.showinfo("Error", "Must be number.")
-    if int(ent_size.get()) < 1 or int(ent_size.get()) > 99:
+    elif int(ent_size.get()) < 1 or int(ent_size.get()) > 99:
         messagebox.showinfo("Error", "Must be 1 - 99 inclusive.")
     elif ent_size.get().isnumeric():
         obstaclepercent = int(ent_size.get()) / 100
@@ -77,50 +90,52 @@ def genobstacles():
                 grid[obstacle % width][obstacle // width].config(bg="Grey")
             except:
                 print(obstacle, obstacle % width, obstacle // width)
+
+    lbl_data.delete("1.0", tk.END)
+    lbl_data.insert(tk.END, obstacles)
+    lbl_data.insert(tk.END, " No. Obs "+ str(len(obstacles)))
         
-
-
-
-
 ##################################################################################################
 # Menu Frame
-lbl_mode = tk.Label(menu, text="Mode")
-lbl_mode.pack()
+def setelements():
+    global ent_size, lbl_obstaclesize, lbl_data
+    lbl_mode = tk.Label(menu, text="Mode")
+    lbl_mode.pack()
 
-rad = tk.Radiobutton(menu, text="Map Editing", padx = 20, variable=v, value=1)
-rad.pack()
+    rad = tk.Radiobutton(menu, text="Map Editing", padx = 20, variable=v, value=1)
+    rad.pack()
 
-rad = tk.Radiobutton(menu, text="Path Search", padx = 20, variable=v, value=2)
-rad.pack()
+    rad = tk.Radiobutton(menu, text="Path Search", padx = 20, variable=v, value=2)
+    rad.pack()
 
-rad = tk.Radiobutton(menu, text="Map Setting", padx = 20, variable=v, value=3)
-rad.pack()
+    rad = tk.Radiobutton(menu, text="Map Setting", padx = 20, variable=v, value=3)
+    rad.pack()
 
-lbl_obstacle = tk.Label(menu, text="Obstacle [%]")
-lbl_obstacle.pack()
+    lbl_obstacle = tk.Label(menu, text="Obstacle [%]")
+    lbl_obstacle.pack()
 
-ent_size = tk.Entry(menu)
-ent_size.pack()
+    ent_size = tk.Entry(menu)
+    ent_size.pack()
 
-btn_generate = tk.Button(menu, text="Generate Map", command=genobstacles)
-btn_generate.pack()
+    btn_generate = tk.Button(menu, text="Generate Map", command=genobstacles)
+    btn_generate.pack()
 
-# btn_clear = tk.Button(menu, text="Clear Map", command=clearmap)
-# btn_clear.pack()
+    # btn_clear = tk.Button(menu, text="Clear Map", command=clearmap)
+    # btn_clear.pack()
 
-lbl_toolbox = tk.Label(menu, text="Obstacles: ")
-lbl_toolbox.pack()
+    lbl_toolbox = tk.Label(menu, text="Obstacles: ")
+    lbl_toolbox.pack()
 
-lbl_obstaclesize = tk.Label(menu, text="")
-lbl_obstaclesize.pack()
+    lbl_obstaclesize = tk.Label(menu, text="")
+    lbl_obstaclesize.pack()
 
-lbl_info = tk.Label(menu, text="Width: " + str(defaultwidth) + " Area: " + str(defaultwidth**2))
-lbl_info.pack()
+    lbl_info = tk.Label(menu, text="Width: " + str(defaultwidth) + " Area: " + str(defaultwidth**2))
+    lbl_info.pack()
 
-##################################################################################################
-# Obstacle Frame
+    lbl_data = tk.Text(dataframe)
+    lbl_data.pack()
 
+setelements()
 makemap()
-
 
 root.mainloop()
